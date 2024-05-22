5c2cae437b144b2b00556dd828705336c6243ab3
VuNgocDiep2004 <nguyenvandonggungun@gmail.com>
2024-05-22 16:39:58 +0700

    bai2

--- baikiemtra/src/VND053PERSON.java
+++ baikiemtra/src/VND053PERSON.java
@@ -1,5 +1,24 @@
-package src.baikiemtra.Javabaidautien;
-public class VND053PERSON;
+
+import java.util.Scanner;
+import java.util.LinkedList;
+public class VND053PERSON {
+public static void main(String[] args) throws Exception {
 String name;
 int tuoi;
 double diem;
+Scanner sc = new Scanner(System.in);
+public void nhapthongtin () {
+    System.out.println("nhap ten : ");
+    name = sc.nextLine();
+    System.out.println("nhap tuoi : ");
+    tuoi = sc.nextInt();
+    System.out.println("nhap diem");
+    diem = sc.nextDouble();
+}
+public void xuatthongtin () {
+    System.out.println("ho va ten : " + name);
+    System.out.println("tuoi : " + tuoi);
+    System.out.println("diem : " + diem);
+}
+}
+}
